a3cf333b0f6598b30083b47a3466819c9336c7a8
Milica Lukovic <milicalukovic2002@gmail.com>
2025-08-04 21:26:49 +0200

    Kreiranje modela i migracija

---
+++ app/Models/Activity.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'name',
+        'price',
+        'duration',
+        'location',
+        'content', // Updated from 'description' to 'content'
+    ];
+}
---
+++ app/Models/PlanItem.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PlanItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'time_from',
+        'time_to',
+        'amount',
+    ];
+}
